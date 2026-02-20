8dcbceb693dc72a8c7b1b0cb4f2a439197390661
chanhuil <qaaqaaqaaa@jbnu.ac.kr>
2026-02-21 03:41:00 +0900

    add: several things

--- Assets/GameHandlerScript.cs
+++ Assets/GameHandlerScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
@@ -27,9 +28,10 @@ public class GameHandlerScript : MonoBehaviour
     
     private List<RandomEvent> randomEvents = new List<RandomEvent>()
     {
-        new RandomEvent("랜덤 이벤트1", "랜덤 이벤트 이다", new int[,] { {1, 0, 0}, {-1, 0, 0} }),
-        new RandomEvent("랜덤 이벤트2", "랜덤 이벤트 2!! 이다", new int[,] { {1, 1, 1}, {-1, -1, -1} }),
-        new RandomEvent("랜덤 이벤트3", "랜덤 이벤트 3!!!!!!! 이다", new int[,] { {3, -3, 0}, {3, 0, -3} }),
+        new RandomEvent("랜덤 이벤트1", "불쑥 하여 지나다 하지만 아내는 제대로 제사를 부르던 뇌물이기 순수하니까, 하얗습니다. 초관리는 그런 사회상이요 장끼를 이야기하다. 듣다 안에 도시를 요약하던 보낼 맡으러 스스로와 작다. 공산당의 노래다, 듣습니다 효자에서 남자가 앉다. 것 있은 언제나 세기를 먹고 같다.", new int[,] { {1, 0, 0}, {-1, 0, 0} }),
+        new RandomEvent("랜덤 이벤트2", "없다 구할까 소식도 출가하는, 외 온다 마련된다. 거 때를 하나가 시리즈에 한다. 무용에 나갑니다 어렵으라 우리나라가 그러나 정직하는 같다. 대행에 있은, 빚어내면서 있는 품이 다시 붙어서 그런데 것 없어. 문화적 미도 5편 확대가 웅크린 맞다 또렷하다 이때에서 식으로 나가게 부르다.", new int[,] { {1, 1, 1}, {-1, -1, -1} }),
+        new RandomEvent("랜덤 이벤트3", "칩니다 오른, 버리다 행정병만 도모되어요. 아니자 알아 아직 오다 것 오다. 웃듯이 비용은 때문 설계도를 물리학회는 개방의 이웃이 시작하다. 목표가 글자를 나가 난타하다 높이게 등 길이 이마로 못 뜨겁고 평가하다. 것 날개가 되면 때를 있다.\n", new int[,] { {0, -3, 0}, {0, 0, -3} }),
+        new RandomEvent("랜덤 이벤트4", "칩니다 오른, 버리다 행정병만 도모되어요. 아니자 알아 아직 오다 것 오다. 웃듯이 비용은 때문 설계도를 물리학회는 개방의 이웃이 시작하다. 목표가 글자를 나가 난타하다 높이게 등 길이 이마로 못 뜨겁고 평가하다. 것 날개가 되면 때를 있다.\n", new int[,] { {-1, 2, 2}, {-3, 5, 5} }),
     };
     private RandomEvent currentRandomEvent;
     
