d696c2ab9723994476b33419ac8668f5a4e47b3d
Ymo0418 <shk9848117@naver.com>
2026-02-21 19:46:35 +0900

    Add aftermath & random Selection

--- Assets/RandomEventScript.cs
+++ Assets/RandomEventScript.cs
@@ -4,32 +4,78 @@ using UnityEngine.UI;
 
 public class RandomEventScript : MonoBehaviour
 {
-    static float[]           rotations = new float[] { 0f * Mathf.Deg2Rad, 0f * Mathf.Deg2Rad, 0f * Mathf.Deg2Rad, -45f * Mathf.Deg2Rad, 45f * Mathf.Deg2Rad };
+    static float[]           rotations = new float[] { 0f * Mathf.Deg2Rad, 0f * Mathf.Deg2Rad, 0f * Mathf.Deg2Rad, -50f * Mathf.Deg2Rad, 50f * Mathf.Deg2Rad };
     public Sprite[]          backgroundSprites;
-    public GameObject        backgroundObject;
-    public Image             backgroundImage;
+    public GameObject[]      backgroundObjects;
+
+    private Image[]          backgroundImage;
     public TextMeshProUGUI   randomEventName;
     public TextMeshProUGUI   randomEventDescription;
-    public TextMeshProUGUI[] DecisionTexts;
-    Button[] buttons;
+    public TextMeshProUGUI[] decisionTexts;
+    Button[]                 buttons;
+
+    Animator                 animator;
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         buttons = GetComponentsInChildren<Button>();
-        backgroundImage = backgroundObject.GetComponentInChildren<Image>();
+
+        backgroundImage = new Image[backgroundObjects.Length];
+        for(int i = 0; i < backgroundObjects.Length; i++)
+        {
+            backgroundImage[i] = backgroundObjects[i].GetComponent<Image>();
+        }
+    }
+
+    public void Update()
+    {
     }
 
     public void SetRandomEvent(Quest randomEvent)
     {
-        backgroundObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, rotations[Random.Range(0, rotations.Length)]));
-        backgroundImage.sprite = backgroundSprites[Random.Range(0, backgroundSprites.Length)];
+        animator.SetBool("RandomEventDisplay", true);
+
+        Quaternion quat = Quaternion.Euler(new Vector3(0, 0, rotations[Random.Range(0, rotations.Length)]));
+
+        var sprite = backgroundSprites[Random.Range(0, backgroundSprites.Length)];
+
+        for (int i = 0; i < backgroundObjects.Length; ++i)
+        {
+            backgroundObjects[i].transform.rotation = quat;
+            backgroundImage[i].sprite = sprite;
+        }
+
         randomEventName.text = randomEvent.QuestName;
         randomEventDescription.text = randomEvent.QuestDialog;
-
-        for (int i = 0; i < DecisionTexts.GetLength(0); i++)
+        for (int i = 0; i < decisionTexts.GetLength(0); i++)
         {
-            DecisionTexts[i].text = randomEvent.selections[i].displayName;
+            decisionTexts[i].text = randomEvent.selections[i].displayName;
             buttons[i].onClick.AddListener(randomEvent.selections[i].Select);
         }
+        CancelInvoke();
+    }
+    
+    public void ShowAftermath(Quest quest, int idx)
+    {
+        //Effect
+        
+        backgroundObjects[0].SetActive(true);
+        backgroundObjects[1].SetActive(false);
+        backgroundObjects[0].GetComponentInChildren<TextMeshProUGUI>().text = quest.Get_AftermathDialog(idx);
+
+        Invoke("CloseRandomEvent", 2f);
+    }
+
+    public void DisableAftermath()
+    {
+        backgroundObjects[0].SetActive(false);
+        backgroundObjects[1].SetActive(true);
+        CancelInvoke();
+    }
+
+    public void CloseRandomEvent()
+    {
+        animator.SetBool("RandomEventDisplay", false);
     }
 }
