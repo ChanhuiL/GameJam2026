bc8f97edb7a8696f50a7c7f6cc613d5529791fd3
Ymo0418 <shk9848117@naver.com>
2026-02-21 21:10:09 +0900

    add background

--- Assets/Scripts/MapCameraMovement.cs
+++ Assets/Scripts/MapCameraMovement.cs
@@ -6,19 +6,19 @@ public class MapCameraMovement : MonoBehaviour
 {
     [Header("Movmenet")]
     public float        movementDrag = 3f;
-    public float        movementSpeed = 4f;
+    public float        movementSpeed = 16f;
 
     [Header("Zoom")]
-    public float        scrollSensitive = 1f;
-    public float        zoomDrag = 1f;
-    public float        minSize = 1f;
-    public float        maxSize = 10f;
+    public float        scrollSensitive = 3f;
+    public float        zoomDrag = 12f;
+    public float        minSize = 8f;
+    public float        maxSize = 25f;
 
     [SerializeField] CinemachineCamera   virtualCam;
     [SerializeField] InputActionProperty scrollAction;
 
     Vector2             vInput = Vector2.zero;
-    float               targetZoomSize = 4f;
+    float               targetZoomSize = 8f;
     bool                isFocusing = false;
 
     private void Awake()
